f4353d9ea175139ec9535afbc7a4c9c15085cb4d
Madhavi Yamani <>
2020-03-17 18:29:34 -0700

    second commit

---
+++ TestingFramework/TestingFramework/Calculate.swift
@@ -0,0 +1,14 @@
+//
+//  Calculate.swift
+//  TestingFramework
+//
+//  Created by Madhavi Yamani on 3/17/20.
+//  Copyright © 2020 info. All rights reserved.
+//
+
+import Foundation
+public final class Calculate {
+    public func add(a: Int, b:Int) -> Int {
+        return a + b
+    }
+}
