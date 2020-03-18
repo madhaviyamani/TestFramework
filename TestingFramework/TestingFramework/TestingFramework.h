06560b664e81cbee176d2f3b34d40c0fb8c1de8c
Madhavi Yamani <>
2020-03-17 18:09:13 -0700

    first commit

---
+++ TestingFramework/TestingFramework/TestingFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestingFramework.h
+//  TestingFramework
+//
+//  Created by Madhavi Yamani on 3/17/20.
+//  Copyright © 2020 info. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestingFramework.
+FOUNDATION_EXPORT double TestingFrameworkVersionNumber;
+
+//! Project version string for TestingFramework.
+FOUNDATION_EXPORT const unsigned char TestingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestingFramework/PublicHeader.h>
+
+
